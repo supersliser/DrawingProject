363478b4fbd95971529604f63b87f634300e89cd
supersliser <thomaslower9@gmail.com>
2023-12-04 17:00:00 +0000

    ghfhgf

--- thing.cpp
+++ thing.cpp
@@ -36,12 +36,12 @@ int main()
     brush.w = 5;
     brush.h = 5;
 
-        colourArea.Draw(renderer, window);
-        CanvasArea.Draw(renderer, window);
-        for (int i = 0; i < 8; i++)
-        {
-            Colours[i].Draw(renderer);
-        }
+    colourArea.Draw(renderer, window);
+    CanvasArea.Draw(renderer, window);
+    for (int i = 0; i < 8; i++)
+    {
+        Colours[i].Draw(renderer);
+    }
     char drawing = 0;
 
     while (!finished)
@@ -49,6 +49,16 @@ int main()
         SDL_Event event; /* an incoming event */
         while (SDL_PollEvent(&event))
         {
+            if (drawing)
+            {
+                if (event.button.y >= canvas.getPosition().y && event.button.y <= canvas.getPosition().y + canvas.getSize().height && event.button.x >= canvas.getPosition().x && event.button.x <= canvas.getPosition().x + canvas.getSize().width)
+                {
+                    SDL_SetRenderDrawColor(renderer, CurrentColour.r, CurrentColour.g, CurrentColour.b, CurrentColour.a);
+                    brush.x = event.button.x - 2;
+                    brush.y = event.button.y - 2;
+                    SDL_RenderFillRect(renderer, &brush);
+                }
+            }
             switch (event.type)
             {
             case SDL_QUIT:
@@ -58,10 +68,10 @@ int main()
                 drawing = 1;
                 for (int i = 0; i < 8; i++)
                 {
-                    if (event.button.x >= Colours[i].getPosition().x && 
-                    event.button.x <= Colours[i].getPosition().x + Colours[i].getSize().width && 
-                    event.button.y >= Colours[i].getPosition().y && 
-                    event.button.y <= Colours[i].getPosition().y + Colours[i].getSize().height)
+                    if (event.button.x >= Colours[i].getPosition().x &&
+                        event.button.x <= Colours[i].getPosition().x + Colours[i].getSize().width &&
+                        event.button.y >= Colours[i].getPosition().y &&
+                        event.button.y <= Colours[i].getPosition().y + Colours[i].getSize().height)
                     {
                         Colours[i].Click(&CurrentColour);
                         printf("current colour is %d, %d, %d, %d", CurrentColour.r, CurrentColour.g, CurrentColour.b, CurrentColour.a);
@@ -71,18 +81,6 @@ int main()
                 break;
             case SDL_MOUSEBUTTONUP:
                 drawing = 0;
-                break;
-            case SDL_MOUSEMOTION:
-                if (event.button.y >= canvas.getPosition().y && event.button.y <= canvas.getPosition().y + canvas.getSize().height && event.button.x >= canvas.getPosition().x && event.button.x <= canvas.getPosition().x + canvas.getSize().width)
-                {
-                    if (drawing)
-                    {
-                        SDL_SetRenderDrawColor(renderer, CurrentColour.r, CurrentColour.g, CurrentColour.b, CurrentColour.a);
-                        brush.x = event.button.x - 2;
-                        brush.y = event.button.y - 2;
-                        SDL_RenderFillRect(renderer, &brush);
-                    }
-                }
                 break;
             }
         }
